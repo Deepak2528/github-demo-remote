d391369aab3b56a11b125e0d5daea50ea61a3d8c
Deepak_Gholap <deepakgholap223@gmail.com>
2022-01-30 09:57:39 +0530

    Added fn Headers to math.py file

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#Addition operation
 def add(x,y):
     pass
+#substraction operation
 def sub(x,y):
     pass
+#multiolication operation
 def mul(x,y):
     pass
+#Devision operation       
 def div(x,y):
     pass
