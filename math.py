617117ae4d1dfe83c6b503d0575328960a977148
Deepak_Gholap <deepakgholap223@gmail.com>
2022-01-30 13:51:59 +0530

    implimented mul() on local main

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def sub(x,y):
     return x-y           #on Feature 1 Branch
 #multiolication operation
 def mul(x,y)
-    return x*y          #on Feature1 branch on remote
+    return x*y          #on local main
 #Devision operation       
 def div(x,y):
     return x/y          #on remote main
